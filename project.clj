f95cb57ad1129f2e2f5e3c5904be787f1089e5bb
Chandan Kumar <cksharma122@gmail.com>
2020-09-06 12:52:46 +0530

    Add when macros

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clo-utils "0.1.2-SNAPSHOT"
-  :description "Some more util functions for clojure"
+(defproject clo-utils "0.2.0-SNAPSHOT"
+  :description "Collection of useful commonly used macros"
   :url "https://github.com/cksharma11/clo-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
--- test/clo_utils/core_test.clj
+++ test/clo_utils/core_test.clj
@@ -9,8 +9,55 @@
     nil (if-nil true 1)
     2 (if-nil true 1 2)))
 
+(deftest if-empty-test
+  (are [x y] (= x y)
+    1 (if-empty [] 1)
+    1 (if-empty #{} 1)
+    1 (if-empty '() 1)
+    2 (if-empty '(1) 1 2)
+    nil (if-empty [1] 1)
+    nil (if-empty #{1} 1)
+    nil (if-empty '(1) 1)))
+
+(deftest if=-test
+  (are [x y] (= x y)
+    1 (if= 1 1
+           1
+           2)
+    2 (if= 2 1
+           1
+           2)
+    nil (if= 2 1
+             1)))
+
 (deftest when-nil-test
   (are [x y] (= x y)
     1 (when-nil nil 1)
     nil (when-nil false 1)
     nil (when-nil true 1)))
+
+(deftest when-not-nil-test
+  (are [x y] (= x y)
+    nil (when-not-nil nil 1)
+    1 (when-not-nil false 1)
+    1 (when-not-nil true 1)))
+
+(deftest when-empty-test
+  (are [x y] (= x y)
+    1 (when-empty [] 1)
+    1 (when-empty #{} 1)
+    1 (when-empty '() 1)
+    nil (when-empty '(1) 1)
+    nil (when-empty [1] 1)
+    nil (when-empty #{1} 1)
+    nil (when-empty '(1) 1)))
+
+(deftest when-not-empty-test
+  (are [x y] (= x y)
+    nil (when-not-empty [] 1)
+    nil (when-not-empty #{} 1)
+    nil (when-not-empty '() 1)
+    1 (when-not-empty '(1) 1)
+    1 (when-not-empty [1] 1)
+    1 (when-not-empty #{1} 1)
+    1 (when-not-empty '(1) 1)))
